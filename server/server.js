763db2b98a1e8677e67935cd53a0c971960d1541
diegocalero0 <diegoacalero0@gmail.com>
2018-06-12 02:27:28 -0500

    working

--- server/server.js
+++ server/server.js
@@ -4,12 +4,12 @@ var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
 server.listen(process.argv[2], function(){
-	console.log("Corriendo en el servidor - puerto: 8080");
+	console.log("Corriendo en el servidor - puerto:" + process.argv[2]);
 });
 
 app.get("/login", function(req, res){
 	var prueba = {"nombre":"diego", "apellido":"calero"};
-	res.json();
+	res.json(prueba);
 });
 
 app.get("/prueba", function(req, res){
